7156ee97e056e868fb8d3fdcda1e340919eeab8b
Nize <veryNyze@gmail.com>
2020-05-03 17:55:28 +0800

    add menu, 2 fonts, and change cursor

---
+++ scripts/Menu.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.set_default_cursor_shape(0)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_PLAY_pressed():
+	get_tree().change_scene("res://scenes/World.tscn")
+
+
+func _on_EXIT_pressed():
+	get_tree().quit() # Replace with function body.
--- scripts/World.gd
+++ scripts/World.gd
@@ -5,7 +5,11 @@ var Bullet = preload('res://scenes/Bullet.tscn')
 
 func _ready():
 	$Player.connect('shoot', self, 'spawn_bullet')
+	Input.set_default_cursor_shape(3)
 
+func _input(event):
+	if event.is_action_pressed('ui_cancel'):
+		get_tree().change_scene("res://scenes/Menu.tscn")
 
 func spawn_bullet(pos, dir):
 	var bullet
